b7bd1cc96eac9854441277165184f1f732767cb2
EDO-MAN <73830965+EDO-MAN@users.noreply.github.com>
2021-04-21 17:42:42 +0900

    변경 & 추가
    배경 변경 & 타일 변경 & 카메라 캐릭터 중심시점에 맵을 벗어 나지 못하게 함

---
+++ Assets/Scripts/LeeDoHyun/KeyFocusMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyFocusMove : MonoBehaviour
+{
+    public float offest;
+    private Vector3 origin;
+    public GameObject player;
+    void Start()
+    {
+        origin = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(origin.x - (offest * player.transform.position.x), origin.y, origin.z);
+    }
+}
