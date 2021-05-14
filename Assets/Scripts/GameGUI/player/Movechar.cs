1019f89eff76baf9ed7c50cb27f315c58af57dcb
Shin-donghyun <66452536+Shin-donghyun@users.noreply.github.com>
2021-05-14 22:25:59 +0900

    추가

--- Assets/Scripts/GameGUI/player/Movechar.cs
+++ Assets/Scripts/GameGUI/player/Movechar.cs
@@ -43,6 +43,8 @@ public class Movechar : MonoBehaviour
             //Attack
             if (Input.GetKey(KeyCode.Z))
             {
+                
+
                 Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(pos.position, boxSize, 0);
                 foreach (Collider2D collider in collider2Ds)
                 {
